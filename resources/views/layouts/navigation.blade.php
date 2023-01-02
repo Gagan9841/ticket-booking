f0b7ddb180c4f968b192f1ea343218331086b83a
gagan9841 <bishwaach@gmail.com>
2023-01-02 22:11:06 +0545

    homepage front-end

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,30 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow mb-2">
+    <div class="container">
+      <div class="navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ticket Rate</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item active">
+            <a class="nav-link auth" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link auth" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
